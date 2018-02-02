f3c4948c0eba11280a18010451700fb02bd1de28
Bernorts <a01209023@itesm.mx>
2018-02-02 07:34:13 -0600

    Poisson, poisson acumulativa, proceso de poisson, factorial y permutación

--- formulario.py
+++ formulario.py
@@ -1,57 +1,118 @@
 #!/usr/bin/python
 # encoding: utf-8
-import math;
+import math
 
-def combination(n,k):
-    f = math.factorial
+def factorial():
+    n = int(input("Introduce el valor de n: "))
+    return factorial_in(n)
+
+def factorial_in(n):
+    acum = 1
+    while True:
+        if ( n == 0 or n == 1):
+            return acum
+        else:
+            acum = acum * n
+            n = n - 1
+
+
+def combination():
+    n = int(input("Introduce el valor de n: "))
+    k = int(input("Introduce el valor de k: "))
+    return combination_in(n,k)
+
+def combination_in(n,k):
+    f = factorial_in
     return f(n) / (f(k) * f(n-k))
+
+def permutation():
+    n = int(input("Introduce el valor de n: "))
+    k = int(input("Introduce el valor de k: "))
+    return permutation_in(n,k)
+
+def permutation_in(n,k):
+    f = factorial_in
+    return f(n) / f(n-k)
 
 
 def cumulativeBinom():
     res = 0;
-    n = input("Introduce el valor de n")
-    p = input("Introduce el valor de p")
-    q = input("Introduce el valor de q")
-    low_limit = input("Introduce el limite inferior")
-    upper_limit = input("Introduce el limite superior")
-    for x in range(low_limit,upper_limit+1):
-        res += combination(n, x) * pow(p,x) * pow(q,n-x)
-    return "Result: " + str(res)
+    n = float(input("Introduce el valor de n: "))
+    p = float(input("Introduce el valor de p: "))
+    q = float(input("Introduce el valor de q: "))
+    low_limit = int(input("Introduce el limite inferior: "))
+    upper_limit = int(input("Introduce el limite superior: "))
+    for x in range(low_limit,upper_limit + 1):
+        res += combination_in(n, x) * pow(p,x) * pow(q,n-x)
+        print(res)
+    return str(res)
 
 
 def binom():
-    n = input("Introduce el valor de n")
-    k = input("Introduce el valor de k")
-    p = input("Introduce el valor de p")
-    q = input("Introduce el valor de q")
-    return "Result: " + str(combination(n, k) * pow(p,k) * pow(q,n-k)) + "\nV: " +str(n*p*q) + "\nDesvesta:" +str( math.sqrt(n*p*q))
+    n = float(input("Introduce el valor de n: "))
+    k = float(input("Introduce el valor de k: "))
+    p = float(input("Introduce el valor de p: "))
+    q = float(input("Introduce el valor de q: "))
+    return str(combination_in(n, k) * pow(p,k) * pow(q,n-k)) + "\nVarianza: " +str(n*p*q) + "\nDesviación estándar:" +str( math.sqrt(n*p*q))
 
-def poissionfunc():
-    return 0
+def poisson():
+    e = math.e
+    x = float(input("Introduce el valor de x: "))
+    l = float(input("Introduce el valor de lambda: "))
+    res = (pow(e, -l) * pow(l, x)) / factorial_in (x)
+    return str(res)
+
 def poissonproc():
-    return 1
+    e = math.e
+    x = float(input("Introduce el valor de x: "))
+    a = float(input("Introduce el valor de alfa: "))
+    t = float(input("Introduce el valor de t (tiempo, área, etc): "))
+    l = a * t
+    res = (pow(e, -l) * pow(l, x)) / factorial_in (x)
+    return str(res)
+
+def cumulativePoisson():
+    res = 0;
+    e = math.e
+    l = float(input("Introduce el valor de lambda: "))
+    low_limit = int(input("Introduce el limite inferior: "))
+    upper_limit = int(input("Introduce el limite superior: "))
+    for x in range(low_limit,upper_limit + 1):
+        res += (pow(e, -l) * pow(l, x)) / factorial_in (x)
+        print(res)
+    return str(res)
+
 
 def printMenu():
-    print("1-Distribución binomial\n")
+    print("1-Distribución Binomial\n")
     print("2-Binomial acumulativo\n")
-    print("2-Distribución poisson\n")
-    print("3-Proceso de poisson\n")
+    print("3-Distribución Poisson\n")
+    print("4-Proceso de Poisson\n")
+    print("5-Poisson acumulativo\n")
+    print("6-Factorial\n")
+    print("7-Combinación\n")
+    print("8-Permutación\n")
 
 def main():
     while (1):
         printMenu();
-        choice = input("\nIntroduce la opción seleccionada \n")
+        choice = int(input("\nIntroduce la opción deseada \n"))
         options = {
-            1 : binom,
-            2 : cumulativeBinom,
-            3 : poissionfunc,
+            1: binom,
+            2: cumulativeBinom,
+            3: poisson,
             4: poissonproc,
+            5: cumulativePoisson,
+            6: factorial,
+            7: combination,
+            8: permutation
         }
+
         if choice in options:
-             print ("\n\nresult"+ options[choice]() + "\n\n")
+             print ("\n\nResultado: "+ str(options[choice]()) + "\n\n")
         else:
              pass
-             print("Not a function")
+             print("\n Esa opción no existe. Elige otra: \n")
 
 
     # my code here
