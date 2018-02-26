098bc0372f4141a307dce2b38208afde23ce8277
MartinMolinero <a01204818@itesm.mx>
2018-02-26 08:04:39 -0600

    formulas

--- accept_reject.rb
+++ accept_reject.rb
@@ -26,16 +26,36 @@ def method
   limit = Integer($stdin.readline(), 10)
   m = decide_max(low, high)
   numbers = []
-  puts "Max #{m}"
+  itr = 0
   while numbers.length < limit do
     r1 = rand().to_f
     r2 = rand().to_f
     x_star = low + (high - low) * r1
+    itr = itr +1
     f_x_star = function(x_star)
-    if r2 <= (f_x_star/m) then
+    if r2 <= (f_x_star.to_f/m.to_f) then
       numbers.push(x_star)
     end
   end
+  puts "Se generaron #{itr} numeros"
+  perc = numbers.length.to_f / itr.to_f
+  puts "Aceptados #{perc}"
+  puts "Rechazados #{1 - perc}"
+  mean = 0
+  variance = 0
+  numbers.each do |n|
+    mean += n
+  end
+  mean = mean.to_f / numbers.length.to_f
+  difference = 0
+  numbers.each do |n|
+    difference += (n - mean) ** 2
+  end
+  difference = difference.to_f / numbers.length.to_f
+
+  puts "Media #{mean}"
+  puts "Varianza #{difference}"
+  puts "Desv esta #{Math.sqrt(difference)}"
   output = File.open("output.txt", "w")
   numbers.each do |n|
     output << n.to_s + "\n"
