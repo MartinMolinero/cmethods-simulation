3f6e138bd7cc4e727fac7b713762d2f0a3a394b8
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-03-15 07:50:09 -0600

    sistema de colas m/m/s

---
+++ sistema_colas_mms.rb
@@ -0,0 +1,75 @@
+class Float
+  def fact
+    (1..self).reduce(:*) || 1
+  end
+end
+
+class Integer
+  def fact
+    (1..self).reduce(:*) || 1
+  end
+end
+
+def lq(miu, lam, ro, s, p_0)
+  ((((lam / miu)**s) * p_0 * ro) / (s.fact * (1 - ro)))
+end
+
+def wq(l_q, lam)
+  l_q / lam
+end
+
+def ws(w_q, miu)
+  w_q + (1 / miu)
+end
+
+def ls(lam, w_s)
+  lam * w_s
+end
+
+def ro(miu, lam, s)
+  lam / (miu * s)
+end
+
+def have_0_clients_system(miu, lam, ro, s)
+  x = 0
+  for n in 0..(s - 1)
+    x += ((lam / miu)**n) / n.fact
+  end
+  y = ((lam / miu)**s) / (s.fact * (1 - ro))
+  1 / (x * y)
+end
+
+def have_n_clients_system(miu, lam, ro, s, p_0, n)
+  if n >= 0 && n <= s
+    (((lam / miu)**n) * p_0) / n.fact
+  elsif n > s
+    (((lam / miu)**n) * p_0) / (s.fact * s**(n-s))
+  end
+end
+
+def main
+  puts "Sistema de colas MMS\n Bernardo Ortega Septien \n Bernardo Gomez Romero \n Martin Alejandro Molinero"
+  puts "Introduce la tasa media de servicio: "
+  miu = Float($stdin.readline())
+  puts "Introduce la tasa media de llegadas: "
+  lam = Float($stdin.readline())
+  puts "Introduce el numero de sistemas: "
+  systems = Float($stdin.readline())
+  puts "Introduce el numero de personas (n) para obtener probabilidad p(n): "
+  n = Float($stdin.readline())
+  ro = ro(miu, lam, systems) # listo
+  p_0 = have_0_clients_system(miu, lam, ro, systems)
+  l_q = lq(miu, lam, ro, systems, p_0)
+  w_q = wq(l_q, lam)
+  w_s = ws(w_q, miu)
+  l_s = ls(lam, w_s)
+  (n == 0) ? p_n = p_0 : p_n = have_n_clients_system(miu, lam, ro, systems, p_0, n)
+
+  puts "Ls: #{l_s.to_s}"
+  puts "Lq: #{l_q.to_s}"
+  puts "Wq: #{w_q.to_s}"
+  puts "Ws: #{w_s.to_s}"
+  puts "Ws: #{p_n.to_s}"
+end
+
+main
