db171c226ae98979dfeefe8e39824cad89e1f357
Bernardo Gómez Romero <a01209704@itesm.mx>
2018-04-16 09:53:52 -0500

    get n and h

--- final_proj.py
+++ final_proj.py
@@ -10,8 +10,9 @@ def rh(rh_prev, miuh, rohi, ihi, gamahi):
 def gh_1(rh, miuh, nht, ch):
     return rh * math.exp(miuh)*nht*(1- ((math.exp(miuh)*nht)/(ch)))
 
-def nh_next_time():
-    return (rh * math.exp(miuh) * nht* (1- ((math.exp(miuh)*nht)/(ch))) + (math.exp(miuh) * nht))
+def nh_next_time(rh, miuh, nht, ch):
+    return (rh * math.exp(miuh) * nht * (1- ((math.exp(miuh)*nht)/(ch))) +
+            (math.exp(miuh) * nht))
 
 def gh_2(miuh, nh):
     return (1- math.exp(miuh) * nh)
@@ -22,28 +23,29 @@ def nh_2_next_time(nh):
 def iFun():
 	i = s * e_power(-miu) * (1 - e_power())
     
-def s_h(g_h, miu, beta, ro, prev_I, prev_S, prev_R):
+def sFun(g_h, miu, beta, rho, prev_I, prev_S, prev_R):
     return (g_h + 
             (e_power(-miu) * e_power(beta * prev_I) * prev_S) + 
-            (prev_R * e_power(-miu) * (1 - e_power(-ro))) )
+            (prev_R * e_power(-miu) * (1 - e_power(-rho))) )
 
 
 def main():
     h = int(input("Introduce el número de poblaciones (h): "))
 
-	for i in range(1,h):
-		n = int(input("Introduce el número inicial de individuos de la población: "))
-		miu = int(input("Introduce el factor miu (probabilidad de mortalidad del virus): "))
-		rho = int(input("Introduce el factor rho: "))
-		beta = int(input("Introduce el factor beta: "))
-		gamma = int(input("Introduce el factor gamma: "))
+    for i in range(1,h):
+        n = int(input("Introduce el número inicial de individuos de la población: "))
+        miu = int(input("Introduce el factor miu (probabilidad de mortalidad del virus): "))
+        rho = int(input("Introduce el factor rho: "))
+        beta = int(input("Introduce el factor beta: "))
+        gamma = int(input("Introduce el factor gamma: "))
 
-		sArray = [0]
-		iArray = [0]
-		rArray = [0]
+    sArray = [0]
+    iArray = [0]
+    rArray = [0]
 
-		for t in range(1, 150):
-			
-
+    for t in range(1, 150):
+        n = nh_next_time(rho, miu, n, ch)
+        g = gh_1(rho, miu, n, ch)
+			        
 main()
 
