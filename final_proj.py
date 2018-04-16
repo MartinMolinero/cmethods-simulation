20190e9d81c0abe823b98b6968a5b034bc2f278a
Bernorts <a01209023@itesm.mx>
2018-04-16 09:50:10 -0500

    I fun

--- final_proj.py
+++ final_proj.py
@@ -3,7 +3,6 @@ import math
 def rh(rh_prev, miuh, rohi, ihi, gamahi):
     return (rh_prev*math.exp(miuh)*math.exp(rohi)) + (ihi*math.exp(miuh)*(1-math.exp(gamahi)))
 
-
 def gh_1(rh, miuh, nht, ch):
     return rh * math.exp(miuh)*nht*(1- ((math.exp(miuh)*nht)/(ch)))
 
@@ -16,15 +15,15 @@ def gh_2(miuh, nh):
 def nh_2_next_time(nh):
     return nh
 
-def iFun():
-	i = s * e_power(-miu) * (1 - e_power())
-
+def iFun(sLast, iLast, beta, gama, miu):
+	return sLast * e_power(-miu) * (1 - e_power(-(beta * iLast))) + (iLast * e_power(-miu) * e_power(-gama))
 
 def main():
 h = int(input("Introduce el número de poblaciones (h): "))
 
 	for i in range(1,h):
 		n = int(input("Introduce el número inicial de individuos de la población: "))
+		ch = int(input("Introduce ch: "))
 		miu = int(input("Introduce el factor miu (probabilidad de mortalidad del virus): "))
 		rho = int(input("Introduce el factor rho: "))
 		beta = int(input("Introduce el factor beta: "))
@@ -35,7 +34,8 @@ h = int(input("Introduce el número de poblaciones (h): "))
 		rArray = [0]
 
 		for t in range(1, 150):
-			
+			sArray.push(sFun)
+			iArray.push(iFun(sArray[t - 1], iArray[t - 1], beta, gama, miu))
 
 main()
 
