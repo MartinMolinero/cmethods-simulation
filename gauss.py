72dea27d7386a86025564a131ee135f350c76742
Bernorts <a01209023@itesm.mx>
2018-04-19 14:26:48 -0500

    Gauss jordan

--- gauss.py
+++ gauss.py
@@ -1,52 +1,81 @@
-import numpy as np
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-def GEPP(A, b, doPricing = True):
-    '''
-    Gaussian elimination with partial pivoting.
 
-    input: A is an n x n numpy matrix
-           b is an n x 1 numpy array
-    output: x is the solution of Ax=b
-            with the entries permuted in
-            accordance with the pivoting
-            done by the algorithm
-    post-condition: A and b have been modified.
-    '''
+def pprint(A):
     n = len(A)
-    if len(b)!= n:
-        raise ValueError("Invalid argument: incompatible sizes between"+
-                         "A & b.", b.size, n)
-    # k represents the current pivot row. Since GE traverses the matrix in the
-    # upper right triangle, we also use k for indicating the k-th diagonal
-    # column index.
+    for i in range(0, n):
+        line = ""
+        for j in range(0, n+1):
+            line += str(A[i][j]) + "\t"
+            if j == n-1:
+                line += "| "
+        print(line)
+    print("")
 
-    # Elimination
-    for k in range(n-1):
-        if doPricing:
-            # Pivot
-            maxindex = abs(A[k:,k]).argmax() + k
-            if A[maxindex, k] == 0:
-                raise ValueError("Matrix is singular.")
-            # Swap
-            if maxindex != k:
-                A[[k,maxindex]] = A[[maxindex, k]]
-                b[[k,maxindex]] = b[[maxindex, k]]
-        else:
-            if A[k, k] == 0:
-                raise ValueError("Pivot element is zero. Try setting doPricing to True.")
-        #Eliminate
-        for row in range(k+1, n):
-            multiplier = A[row,k]/A[k,k]
-            A[row, k:] = A[row, k:] - multiplier*A[k, k:]
-            b[row] = b[row] - multiplier*b[k]
-    # Back Substitution
-    x = np.zeros(n)
-    for k in range(n-1, -1, -1):
-        x[k] = (b[k] - np.dot(A[k,k+1:],x[k+1:]))/A[k,k]
+
+def gauss(A):
+    n = len(A)
+
+    for i in range(0, n):
+        # Search for maximum in this column
+        maxEl = abs(A[i][i])
+        maxRow = i
+        for k in range(i+1, n):
+            if abs(A[k][i]) > maxEl:
+                maxEl = abs(A[k][i])
+                maxRow = k
+
+        # Swap maximum row with current row (column by column)
+        for k in range(i, n+1):
+            tmp = A[maxRow][k]
+            A[maxRow][k] = A[i][k]
+            A[i][k] = tmp
+
+        # Make all rows below this one 0 in current column
+        for k in range(i+1, n):
+            c = -A[k][i]/A[i][i]
+            for j in range(i, n+1):
+                if i == j:
+                    A[k][j] = 0
+                else:
+                    A[k][j] += c * A[i][j]
+
+    # Solve equation Ax=b for an upper triangular matrix A
+    x = [0 for i in range(n)]
+    for i in range(n-1, -1, -1):
+        x[i] = A[i][n]/A[i][i]
+        for k in range(i-1, -1, -1):
+            A[k][n] -= A[k][i] * x[i]
     return x
 
+
 if __name__ == "__main__":
-    A = np.array([[1.,-1.,1.,-1.],[1.,0.,0.,0.],[1.,1.,1.,1.],[1.,2.,4.,8.]])
-    b =  np.array([[14.],[4.],[2.],[2.]])
-    print(GEPP(np.copy(A), np.copy(b), doPricing = False))
-    print(GEPP(A,b))
+    from fractions import Fraction
+    n = input()
+
+    A = [[0 for j in range(n+1)] for i in range(n)]
+
+    # Read input data
+    for i in range(0, n):
+        line = map(Fraction, raw_input().split(" "))
+        for j, el in enumerate(line):
+            A[i][j] = el
+    raw_input()
+
+    line = raw_input().split(" ")
+    lastLine = map(Fraction, line)
+    for i in range(0, n):
+        A[i][n] = lastLine[i]
+
+    # Print input
+    pprint(A)
+
+    # Calculate solution
+    x = gauss(A)
+
+    # Print result
+    line = "Result:\t"
+    for i in range(0, n):
+        line += str(x[i]) + "\t"
+    print(line)
